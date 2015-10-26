fdf99d2846a5327e6127e3c512df7a910564350a
Felippe Nardi <felippe@nardi.me>
2015-10-26 20:51:25 -0200

    Fix: jasmine tests not running

--- karma.conf.js
+++ karma.conf.js
@@ -24,14 +24,14 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
